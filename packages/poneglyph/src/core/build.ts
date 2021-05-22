85dea436ae962a9a1f89ea2b8b0863a487c270d0
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-05-22 18:36:55 +0800

    Remove `createPagesMap`

--- packages/poneglyph/src/core/build.ts
+++ packages/poneglyph/src/core/build.ts
@@ -1,7 +1,6 @@
 import { DEFAULT_BUILD_OPTIONS } from '../constants';
 import buildBrowserBundles from './build-browser-bundles';
 import buildServerBundle from './build-server-bundle';
-import createPagesMap from './create-pages-map';
 import { BuildFullOptions, BuildOptions } from './types';
 
 export default async function createBuild(config: BuildOptions): Promise<void> {
@@ -11,7 +10,6 @@ export default async function createBuild(config: BuildOptions): Promise<void> {
   };
 
   await Promise.all([
-    createPagesMap(fullConfig),
     buildBrowserBundles(fullConfig, 'production'),
     buildBrowserBundles(fullConfig, 'development'),
     buildServerBundle(fullConfig, 'production'),
