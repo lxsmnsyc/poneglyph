90740623758658239a9473d68f505674e18621ba
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-05-24 00:04:24 +0800

    Update index.ts

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,5 +1,6 @@
 export type { LinkProps, BaseAnchorAttributes } from './Link';
 export { default as Link } from './Link';
 export type { RouterProps } from './Router';
-export { default as Router, useRouter } from './Router';
+export { default as Router } from './Router';
+export { useRouter } from './use-router';
 export * from './router-node';
