f9a3de7d7afeedd3dfda77e23755595357c55bc1
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-05-26 16:49:02 +0800

    Fix server.js

--- examples/demo/server.js
+++ examples/demo/server.js
@@ -2,29 +2,29 @@ import { createServer } from 'poneglyph/server';
 import { createNodeAdapter } from 'poneglyph-adapter-node';
 import { readFile } from 'fs/promises';
 
-const serverManifest = JSON.parse(
-  await readFile(
-    new URL('./dist/server/manifest.json', import.meta.url)
-  )
-);
-const clientManifest = JSON.parse(
-  await readFile(
-    new URL('./dist/client/manifest.json', import.meta.url)
-  )
-);
+// const serverManifest = JSON.parse(
+//   await readFile(
+//     new URL('./dist/server/manifest.json', import.meta.url)
+//   )
+// );
+// const clientManifest = JSON.parse(
+//   await readFile(
+//     new URL('./dist/client/manifest.json', import.meta.url)
+//   )
+// );
 
 createServer({
   env: process.env.NODE_ENV,
-  prod: {
-    handle: createNodeAdapter({
-      port: 3000,
-      assets: './dist/client/assets',
-    }),
-    build: {
-      server: () => import(serverManifest['src/entry-server.tsx'].file),
-      client: clientManifest['src/entry-client.tsx'].file,
-    }
-  },
+  // prod: {
+  //   handle: createNodeAdapter({
+  //     port: 3000,
+  //     assets: './dist/client/assets',
+  //   }),
+  //   build: {
+  //     server: () => import('./dist/server/' + serverManifest['src/entry-server.tsx'].file),
+  //     client: clientManifest['src/entry-client.tsx'].file,
+  //   }
+  // },
   dev: {
     port: 3000,
     build: {
